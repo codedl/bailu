3d3f29c02132f8422262b6ef714441bcfec01278
dl <704490514@qq.com>
2019-11-27 10:37:16 +0800

    1127

--- MyApplication/app/src/main/java/com/example/appzhuzhuang/Action.java
+++
@@ -1,9 +0,0 @@
-package com.example.appzhuzhuang;
-
-public class Action {
-    public final static String BLE_SCAN_FOUND =
-            "com.example.ble.ACTION_SCAN_FOUND";
-    public final static String BT_DISCOVERED =
-            "com.example.bt.BT_DISCOVERED";
-    public final static String DEVICE = "devicestr";
-}
